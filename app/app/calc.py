c3399347f7e39f4f6e253ba520bdef79236c1e9c
Nwike Vincent Ikechukwu <111094354+vinco99@users.noreply.github.com>
2023-10-30 14:40:52 +0100

    More updates

---
+++ app/app/calc.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Simple calculator app
+"""
+
+
+def add(x, y):
+    """Add two numbers together"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and return value"""
+    return y - x
+
+
+# def multiply(x, y):
+#     """Multiply two numbers together"""
+#     return x * y
+
+
+# def divide(x, y):
+#     """Divide x from y and return value"""
+#     return y / x
+
---
+++ app/app/tests.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Simple tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc app"""
+
+    def test_add_numbers(self):
+        """Test that two numbers are added together"""
+        res = calc.add(3, 8)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test that values are subtracted and returned"""
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, 5)
